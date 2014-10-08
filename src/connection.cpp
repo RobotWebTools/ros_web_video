103faaa7e1f23e33d34021b529bdcfff487d7f3c
Mitchell Wills <mwills@wpi.edu>
2014-10-08 09:44:50 -0400

    Fixed ROS_INFO argument mismatch

--- src/connection.cpp
+++ src/connection.cpp
@@ -573,7 +573,7 @@ void connection::handleRead(const boost::system::error_code& e,
           if (!is)
           {
             reply_ = reply::stock_reply(reply::not_found);
-            ROS_INFO("Http request from client %s: %s - file not found ", remote_ip.c_str(), request_.uri.c_str(), full_path.c_str());
+            ROS_INFO("Http request from client %s: %s - file not found", remote_ip.c_str(), full_path.c_str());
             return;
           }
 
